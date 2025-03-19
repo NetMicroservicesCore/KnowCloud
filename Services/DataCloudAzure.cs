9cf7e2e756032e4481c2523d83a60ad128fe93c2
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2025-03-19 17:02:55 -0600

    se crean  los primeros elementos de subida, para trabajar con el blobstorage.

--- Services/DataCloudAzure.cs
+++ Services/DataCloudAzure.cs
@@ -1,6 +1,27 @@
-﻿namespace KnowCloud.Services
+﻿using Azure.Storage.Blobs;
+
+namespace KnowCloud.Services
 {
     public class DataCloudAzure
     {
+        private string connectionString;
+
+        public DataCloudAzure(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("AzureStorage");
+        }
+
+
+        public async Task<string> UpLoadFiles(string container, IEnumerable<IFormFile> files)
+        {
+            var client = new BlobContainerClient(connectionString,container);
+            await client.CreateIfNotExistsAsync();
+            return "text1";
+        }
+
+        public Task Delete(string path, string container)
+        {
+            throw new NotImplementedException(); ;
+        }
     }
 }
