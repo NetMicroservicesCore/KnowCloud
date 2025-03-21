35f50e2bda87698d902c8fc6267f21ec2e3a897f
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2025-03-20 19:44:04 -0600

    agregamos el metodo que se encargara de subir el archivo a el storage de azure

--- Services/DataCloudAzure.cs
+++ Services/DataCloudAzure.cs
@@ -1,4 +1,5 @@
 ﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
 
 namespace KnowCloud.Services
 {
@@ -10,13 +11,23 @@ namespace KnowCloud.Services
         {
             connectionString = configuration.GetConnectionString("AzureStorage");
         }
-
-
         public async Task<string> UpLoadFiles(string container, IEnumerable<IFormFile> files)
         {
             var client = new BlobContainerClient(connectionString,container);
             await client.CreateIfNotExistsAsync();
-            return "text1";
+            client.SetAccessPolicy(Azure.Storage.Blobs.Models.PublicAccessType.Blob);
+            var task = files.Select(async file =>
+            {
+                //obtenemos el nombre del archivo
+                var filaNameOrigin = Path.GetFileName(file.FileName);
+                var extension = Path.GetExtension(file.FileName);
+                var nameFile = $"{Guid.NewGuid()}{extension}";
+                var blob = client.GetBlobClient(nameFile);
+                var blobHttpHeaders = new BlobHttpHeaders();
+                blobHttpHeaders.ContentType = file.ContentType;
+                await blob.UploadAsync(file.OpenReadStream(),blobHttpHeaders);
+
+            });
         }
 
         public Task Delete(string path, string container)
