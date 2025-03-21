21e5180f8d64f44a37b9dee0ad9169e309062bd8
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2025-03-21 00:37:05 -0600

    se modifica el controlador de peticiones para subir el archivo.

---
+++ Controllers/FilesCloudController.cs
@@ -0,0 +1,16 @@
+﻿using KnowCloud.Contract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KnowCloud.Controllers
+{
+    [Route("api/archivos")]
+    public class FilesCloudController : ControllerBase
+    {
+        private readonly IDataCloudAzure cloudAzure;
+        private readonly string container = "archivosadjuntos";
+        public FilesCloudController(IDataCloudAzure _cloudAzure)
+        {
+            this.cloudAzure = _cloudAzure;
+        }
+    }
+}
