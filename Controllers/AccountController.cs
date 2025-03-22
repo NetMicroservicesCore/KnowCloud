bc3f75fa75555cd0979d519b278637087dc660cc
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2025-03-22 16:38:58 -0600

    se genera la interfaz grafica de usuario para complementar su perfil de acceso y datos pertenecientes a la plataforma de aprendizaje.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KnowCloud.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Perfil()
+        {
+            return View();
+        }
+    }
+}
