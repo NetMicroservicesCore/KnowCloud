be9ea4908dca46e241f1edff9fb5084bb80600d3
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2025-03-23 17:53:08 -0600

    Agregamos losprimeros elementos de interfaz de usuario para generar un login de acceso.

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,10 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 namespace KnowCloud.Controllers
 {
     public class AccountController : Controller
     {
+        [HttpGet]
         public IActionResult Perfil()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public  IActionResult Login() 
         {
             return View();
         }
