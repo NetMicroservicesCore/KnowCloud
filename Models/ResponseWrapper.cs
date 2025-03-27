f8d43c1c4dc7ca42967e036626be32f216390f2e
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2025-03-26 21:56:13 -0600

    se agregan entidades para la transferencia y recepcion de datos desde un recurso externo o api

---
+++ Models/ResponseWrapper.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+
+namespace KnowCloud.Models
+{
+    public class ResponseWrapper<T>
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public string Message { get; set; }
+
+        public T Data { get; set; }
+    }
+}
