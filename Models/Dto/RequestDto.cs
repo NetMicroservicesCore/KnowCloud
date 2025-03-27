45acc7693eaa9891a946172c7a3ad62af7cc24f5
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2025-03-26 22:49:15 -0600

    se agregan  requestDto y ResponseDto como parte de la arquitectura propuesta

---
+++ Models/Dto/RequestDto.cs
@@ -0,0 +1,15 @@
+﻿using KnowCloud.Utility;
+using static KnowCloud.Utility.Utilities;
+
+namespace KnowCloud.Models.Dto
+{
+    public class RequestDto
+    {
+        public ApiType ApiType { get; set; } = ApiType.GET;
+        public string Url { get; set; }
+        public object Data { get; set; }
+        public string AccessToken { get; set; }
+
+        public ContentType ContentType { get; set; } = ContentType.Json;
+    }
+}
---
+++ Services/Contract/IAuthServices.cs
@@ -0,0 +1,10 @@
+﻿using KnowCloud.Models;
+using KnowCloud.Models.Dto;
+
+namespace KnowCloud.Services.Contract
+{
+    public interface IAuthServices
+    {
+        Task<ResponseWrapper<LoginResponseDto>> LoginAsync(LoginRequestDto loginRequestDto);
+    }
+}
