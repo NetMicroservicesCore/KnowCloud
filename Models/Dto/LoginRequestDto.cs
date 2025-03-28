cbd603318dff627e1677d4e72af508fa5866707c
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2025-03-28 15:02:41 -0600

    se agregan elementos de validacion para la interfaz grafica,

--- Models/Dto/LoginRequestDto.cs
+++ Models/Dto/LoginRequestDto.cs
@@ -1,8 +1,12 @@
-﻿namespace KnowCloud.Models.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KnowCloud.Models.Dto
 {
     public class LoginRequestDto
     {
+        [Required(ErrorMessage ="Es necesario ingresar el nombre del usuario")]
         public string UserName { get; set; }
+        [Required(ErrorMessage ="Es necesario ingresar un password")]
         public string Password { get; set; }
     }
 }
