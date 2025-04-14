6fc994fb6e167c25fcc55c265f45671845d00f7d
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2025-04-13 23:15:32 -0600

    creamos nuestro CouponService sin ninguna logica de trabajo aun

---
+++ Services/CouponService.cs
@@ -0,0 +1,38 @@
+﻿using KnowCloud.Contract;
+using KnowCloud.Models.Dto;
+
+namespace KnowCloud.Services
+{
+    public class CouponService : ICouponService
+    {
+        public Task<ResponseDto> CreateCouponsAsync(CouponDto couponDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto> DeleteCouponsAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto> GetAllCouponsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto> GetCouponAsync(string couponCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto> GetCouponByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto> UpdateCouponsAsync(CouponDto couponDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
