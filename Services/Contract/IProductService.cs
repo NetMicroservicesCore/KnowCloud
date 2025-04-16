c0e7d082de64f0906e596ecadaa0178ac64f7d04
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2025-04-15 22:28:40 -0600

    se genera la interface IProductService con los metodos necesarios para consultar o consumir el endpoint

--- Services/Contract/IProductService.cs
+++ Services/Contract/IProductService.cs
@@ -1,6 +1,13 @@
-﻿namespace KnowCloud.Services.Contract
+﻿using KnowCloud.Models.Dto;
+
+namespace KnowCloud.Services.Contract
 {
-    public class IProductService
+    public interface IProductService
     {
+        Task<ResponseDto> GetAllProductsAsync();
+        Task<ResponseDto> GetProductByIdAsync(int id);
+        Task<ResponseDto> CreateProductsAsync(ProductDto productDto);
+        Task<ResponseDto> UpdateProductsAsync(ProductDto productDto);
+        Task<ResponseDto> DeleteProductsAsync(int id);
     }
 }
