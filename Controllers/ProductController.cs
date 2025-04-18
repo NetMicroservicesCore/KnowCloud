782cc8ccd77a506420cc4a3615ba7ee32c0409bc
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2025-04-17 20:00:06 -0600

    se agrega el controlador de producto , para consumir el endpoint

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using KnowCloud.Services.Contract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KnowCloud.Controllers
+{
+    public class ProductController : Controller
+    {
+        
+        private readonly IProductService _productService;
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
