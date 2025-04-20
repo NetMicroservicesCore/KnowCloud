5d846dd98c2e6cbc3ec80007ce68e4c298c3f468
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2025-04-20 01:49:24 -0600

    se agregan  los elementos base para cartcontroller

---
+++ Controllers/CartController.cs
@@ -0,0 +1,21 @@
+﻿using KnowCloud.Services.Contract;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KnowCloud.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ICartService _cartService;
+        public CartController(ICartService cartService)
+        {
+            _cartService = cartService;
+        }
+
+        [Authorize]
+        public async Task<IActionResult> CartIndex()
+        {
+            return View();
+        }
+    }
+}
