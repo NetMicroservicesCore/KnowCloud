a4d384af23af4e56a8dd8a7899d78d8e7479c57f
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2025-04-27 03:38:13 -0600

    agrgeamos las clases de servicio

--- Services/Contract/IOrderService.cs
+++ Services/Contract/IOrderService.cs
@@ -4,11 +4,6 @@ namespace KnowCloud.Services.Contract
 {
     public interface IOrderService
     {
-        Task<ResponseDto> GetCouponAsync(string couponCode);
-        Task<ResponseDto> GetAllCouponsAsync();
-        Task<ResponseDto> GetCouponByIdAsync(int id);
-        Task<ResponseDto> CreateCouponsAsync(CouponDto couponDto);
-        Task<ResponseDto> UpdateCouponsAsync(CouponDto couponDto);
-        Task<ResponseDto> DeleteCouponsAsync(int id);
-    }
+        Task<ResponseDto> CreateOrder(CartDto cartDto);
+        
 }
--- Services/OrderService.cs
+++ Services/OrderService.cs
@@ -5,32 +5,7 @@ namespace KnowCloud.Services
 {
     public class OrderService : IOrderService
     {
-        public Task<ResponseDto> CreateCouponsAsync(CouponDto couponDto)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<ResponseDto> DeleteCouponsAsync(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<ResponseDto> GetAllCouponsAsync()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<ResponseDto> GetCouponAsync(string couponCode)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<ResponseDto> GetCouponByIdAsync(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<ResponseDto> UpdateCouponsAsync(CouponDto couponDto)
+        public Task<ResponseDto> CreateOrder(CartDto cartDto)
         {
             throw new NotImplementedException();
         }
