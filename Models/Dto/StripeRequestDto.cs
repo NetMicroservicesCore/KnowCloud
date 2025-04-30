4b656b257fbcf135f91938372d912f849fbf3260
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2025-04-30 16:58:15 -0600

    Agregamos las propiedades de la entidad StripeRequestDto.

--- Models/Dto/StripeRequestDto.cs
+++ Models/Dto/StripeRequestDto.cs
@@ -2,5 +2,13 @@
 {
     public class StripeRequestDto
     {
+        public string StripeSessionUrl { get; set; }
+        public string StripeSessionId { get; set; }
+
+        public string ApprovedUrl { get; set; }
+        
+        public string CancelUrl { get; set; }
+
+        public OrderHeaderDto OrderHeaderDto { get; set; }
     }
 }
