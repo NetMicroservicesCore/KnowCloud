8da5dcc84de0f5a0da39cc1acbe19519f6a23b9c
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2025-05-10 03:05:40 -0600

    agrgeamos el controlador de Ordenes de compra  diseñando el primer metodo Index

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KnowCloud.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
