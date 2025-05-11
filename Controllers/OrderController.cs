56722a4add0ffef2b56e76beb39eeac95126302e
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2025-05-11 00:51:03 -0600

    empezamos a modificar el controlador del tipo order de la aplicacion web.

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -1,9 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using KnowCloud.Services.Contract;
+using Microsoft.AspNetCore.Mvc;
 
 namespace KnowCloud.Controllers
 {
     public class OrderController : Controller
     {
+
+        private readonly IOrderService _orderService;
+
+
         public IActionResult Index()
         {
             return View();
