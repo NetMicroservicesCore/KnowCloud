b289d3dc23d918b1fb178626ed2174df7d43c3bd
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2025-05-18 17:37:09 -0600

    fix:agregamos script para  consultar todos los datos de los productos y asi mostrarlos en el jtable.

---
+++ wwwroot/js/product.js
@@ -0,0 +1,28 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    var url = window.location.search;
+    loadDataTable();
+});
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        order: [[0, 'desc']],
+        "ajax": { url: "/api/Product"},
+        "columns": [
+            { data: 'productId', "width": "5%" },
+            { data: 'name', "width": "25%" },
+            { data: 'price', "width": "20%" },
+            { data: 'description', "width": "10%" },
+            { data: 'categoryName', "width": "10%" },
+            {
+                data: 'productId',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group">
+                    <a href="/api/Product/ProductEdit?productId=${data}" class="btn btn-primary mx-2"><i class="fas fa-edit"></i></a>
+                    </div>`
+                },
+                "width": "10%"
+            }
+        ],
+    })
+}
